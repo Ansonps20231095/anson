4bd5c3b94a7af2b5cecf1dd01832e00c42f2599d
Ansonps20231095 <50223546+Ansonps20231095@users.noreply.github.com>
2019-05-24 09:08:52 +0800

    Add files via upload

--- CARmain.py
+++ CARmain.py
@@ -19,7 +19,7 @@ def lookupColor(color,i):
         strip[i] = (255,179,0)
     # yellow
     elif color == 12:
-        strip[i] = (255,255,0)
+        strip[i] = (55,55,0)
     # green
     elif color == 13:
         strip[i] = (0,255,0)
@@ -28,19 +28,22 @@ def lookupColor(color,i):
         strip[i] = (0,0,255)
     # violet
     elif color == 15:
-        strip[i] = (62,0,105)
+        strip[i] = (62,0,205)
     # red
     elif color == 17:
         strip[i] = (255,0,0)
+
     # light grey
-    elif color == 20
-        strip[i] = (203,203,203)
+    elif color == 20:
+      strip[i] = (203,203,203)
+
     # white
-    elif color == 40
-        strip[i] = (255,255,255)
+    elif color == 40:
+      strip[i] = (255,255,255)
+
     # grey
-    elif color == 30
-        strip [i] = (90,90,90)
+    elif color == 30:
+      strip [i] = (90,90,90)
 
 
 # ----------------------------------------------------------------------------------------------------
@@ -48,34 +51,32 @@ def lookupColor(color,i):
 # ----------------------------------------------------------------------------------------------------
 
 
-def showMessageD(nextMessage,duration,myMessage,startingPoint):
+def showMessageL(nextMessage,duration,myMessage,startingPoint):
     # start - show Message
     if globalvariables.myStart == 0:
         globalvariables.myStart = 1
         globalvariables.countToMaxLength = startingPoint
 
-
-
     i = 0
-    for r in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
-        for c in range(0,8):
-            temp = r % len(myMessage)
-            color = myMessage[temp][c]
+    for r in range(0,8):
+        for c in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
+            temp = c % len(myMessage[0])
+            color = myMessage[r][temp]
             lookupColor(color,i)
             i += 1
 
-    for r in range(globalvariables.countToMaxLength,globalvariables.countToMaxLength+8):
-        for c in range(8,16):
-            temp = r % len(myMessage)
-            color = myMessage[temp][c]
+    for r in range(0,8):
+        for c in range(globalvariables.countToMaxLength+8,globalvariables.countToMaxLength+16):
+            temp = c % len(myMessage[0])
+            color = myMessage[r][temp]
             lookupColor(color,i)
             i += 1
 
     strip.write()
 
-    globalvariables.countToMaxLength -= 1
-    globalvariables.durationCounter -= 1
-    if globalvariables.durationCounter > duration:
+    globalvariables.countToMaxLength += 1
+    globalvariables.durationCounter += 1
+    if globalvariables.countToMaxLength > duration:
         globalvariables.countToMaxLength = 0
         globalvariables.myStart = 0
         globalvariables.durationCounter = 0
@@ -91,20 +92,20 @@ def showMessageD(nextMessage,duration,myMessage,startingPoint):
 while True:
     if globalvariables.messageID == 1:
         # Call function - showMessage(nextMessageID, messageDuration, message, start)
-        showMessageD(2,100,globalvariables.message1,0)
+        showMessageL(2,16,globalvariables.message1,0)
 
     elif globalvariables.messageID == 2:
         # Call function - showMessage(nextMessageID, messageDuration, message, start)
-        showMessageL(3,100,globalvariables.message2,0)
+        showMessageL(3,16,globalvariables.message2,0)
 
     elif globalvariables.messageID == 3:
         # Call function - showMessage(nextMessageID, messageDuration, message, start)
-        showMessageL(4,100,globalvariables.message3,0)
+        showMessageL(4,16,globalvariables.message3,0)
 
     elif globalvariables.messageID == 4:
         # Call function - showMessage(nextMessageID, messageDuration, message, start)
-        showMessageL(5,100,globalvariables.message4,0)
+        showMessageL(5,16,globalvariables.message4,0)
 
     elif globalvariables.messageID == 5:
         # Call function - showMessage(nextMessageID, messageDuration, message, start)
-        showMessageL(1,100,globalvariables.message5,0)
+        showMessageL(1,16,globalvariables.message5,0)
